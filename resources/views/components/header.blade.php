9f1af7678565f5c4029daec72f980940ade566e6
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-02-09 16:21:54 +0100

    Aggiunta componenti

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,40 @@
 <header>
-    <h1>Hello from Header</h1>
+    <div class="container">
+        <nav>
+            <div class="nav-img">
+                <a href="#">
+                    <img src="{{asset('img/dc-logo.png')}}" alt="">
+                </a>
+            </div>
+            <div class="nav-links">
+                <ul>
+                    @foreach ($links as $link)
+                        @if ($link['selected'] == true)
+                            <li class=" mx-2 fs-5 fw-bold"><a class="selected link" href="#">{{strtoupper($link['name'])}}</a></li>
+                        @else
+                            <li class=" mx-2 fs-5 fw-bold"><a class="link" href="#">{{strtoupper($link['name'])}}</a></li>
+                        @endif
+                        @if($link['dropdown'])
+                            <div class="dropdown">
+                                <i class="fa-solid fa-angel-down dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                  <li><a class="dropdown-item" href="#">Action</a></li>
+                                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                          </div>
+                        @endif
+                    @endforeach
+                </ul>
+            </div>
+            <div class="nav_search">
+                <div class="col-auto">
+                    <div class="input-group border-bottom border-primary border-3">
+                        <input type="text" class="form-control border-0" id="autoSizingInputGroup" placeholder="Search">
+                        <div class="input-group-text border-0 bg-white"><i class="fa-solid fa-magnifying-glass"></i></div>
+                    </div>
+                    <label class="visually-hidden" for="autoSizingInputGroup">Username</label>
+                </div>
+        </nav>
+    </div>
 </header>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.main-layout')
 @section('head')
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <h1>ma siamo sicuri??</h1>
 @endsection
