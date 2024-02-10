2493e7bd03c5e913736c7efed417b742cd90e2aa
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-02-10 16:19:28 +0100

    modifica main layout

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -5,15 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite("resources/js/app.js") 
-
     @yield('head')
 </head>
 <body>
-    <div class="text-center">
-        @include('components.header')
-        @include('components.jumbotron')
-        @include('components.comics')
-        @include('components.footer')
-    </div>
 </body>
 </html>
